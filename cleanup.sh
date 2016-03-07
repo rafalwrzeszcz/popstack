1b0c89d7c6b9d5e9da41fa90e23582f26f657353
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-07 09:01:20 +0100

    C++ REST SDK bundle.

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+##
+# This file is part of the PopStack (C++ implementation).
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+##
+
+sudo apt-get remove --purge -y libboost1.55-all-dev libssl-dev cmake
+sudo apt-get autoremove --purge -y
