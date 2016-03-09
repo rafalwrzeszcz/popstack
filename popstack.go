8605c5fbb8b6386a1be2f3256d8e53e39ac8b9b4
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-09 11:44:25 +0100

    Fetching answer.

--- popstack.go
+++ popstack.go
@@ -7,5 +7,53 @@
 
 package main
 
+import "encoding/json"
+import "fmt"
+import "io/ioutil"
+import "net/http"
+import "strconv"
+
+func parse(content []byte) map[string]interface{} {
+    var data map[string]interface{}
+    _ = json.Unmarshal(content, &data)
+    return data
+}
+
+func fetch(call string) map[string]interface{} {
+    var response *http.Response
+    var buffer []byte
+
+    response, _ = http.Get("http://api.stackexchange.com/2.2/" + call + "&site=stackoverflow")
+    buffer, _ = ioutil.ReadAll(response.Body)
+    response.Body.Close()
+    return parse(buffer)
+}
+
+func extractSnippet(body string) string {
+    fmt.Println(body)
+    //TODO: match, extract, unescape, trim
+    return ""
+}
+
 func main() {
+    var response map[string]interface{} = fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany")
+    var items []interface{} = response["items"].([]interface{})
+    var item interface{}
+    var data map[string]interface{}
+    var answer interface{}
+    var exists bool
+    for _, item = range items {
+        data = item.(map[string]interface{})
+        answer, exists = data["accepted_answer_id"]
+        if exists {
+            response = fetch("answers/" + strconv.FormatFloat(answer.(float64), 'f', -1, 64) + "?filter=withbody")
+            item = response["items"].([]interface{})[0]
+            fmt.Println(extractSnippet(item.(map[string]interface{})["body"].(string)))
+
+            //TODO: first make sure there was a snippet extracted
+            break
+        }
+    }
+
+    //TODO; process more pages maybe?
 }
