09747a627723425a3bb4e4be44e1e6a09980d2bb
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-09 11:44:10 +0100

    Initial files.

---
+++ build.sh
@@ -0,0 +1,10 @@
+##
+# This file is part of the PopStack (Go implementation).
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+##
+
+go build \
+    -o popstack \
+    popstack.go
---
+++ popstack.go
@@ -0,0 +1,11 @@
+/**
+ * This file is part of the PopStack (Go implementation).
+ *
+ * @license http://mit-license.org/ The MIT license
+ * @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+ */
+
+package main
+
+func main() {
+}
