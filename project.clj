75c288bb2f82cd2fdce75ef0b2c1bc2ef4031088
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-10 10:59:37 +0100

    JSON unserialization (clj-http for some reason doesn't work).

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
 (defproject popstack "0.1.0-SNAPSHOT"
     :dependencies [
         [org.clojure/clojure "1.7.0"],
+        [org.clojure/data.json "0.2.6"],
         [clj-http "2.1.0"]
     ]
     :main ^:skip-aot popstack.core
--- src/popstack/core.clj
+++ src/popstack/core.clj
@@ -7,13 +7,14 @@
 
 (ns popstack.core
     (:gen-class)
-    (:require [clj-http.client :as client]))
+    (:require [clj-http.client :as client])
+    (:require [clojure.data.json :as json]))
 
 (defn fetch
     [call]
-    (:body (client/get (str "http://api.stackexchange.com/2.2/" call "&site=stackoverflow"))))
+    (json/read-str (:body (client/get (str "http://api.stackexchange.com/2.2/" call "&site=stackoverflow")))))
 
 (defn -main
     [& args]
     (println
-        (fetch "similar?order=desc&sort=relevance&title=Hibernate+manytomany")))
+        (get (fetch "similar?order=desc&sort=relevance&title=Hibernate+manytomany") "items")))
