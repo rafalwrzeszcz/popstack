bab56e04926994077d699711f7f8ab5d5c90bb4b
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-11 11:03:57 +0100

    Fetching JSON response.

--- src/main/java/popstack/Popstack.java
+++ src/main/java/popstack/Popstack.java
@@ -7,8 +7,22 @@
 
 package popstack;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.json.JSONObject;
+
 public class Popstack {
-    public static void main(String[] args) {
-        System.out.println("tesT");
+    public static JSONObject fetch(String call) throws UnirestException {
+        HttpResponse<JsonNode> response = Unirest.get("http://api.stackexchange.com/2.2/" + call + "&site=stackoverflow")
+//            .header("accept", "application/json")
+            .asJson();
+        return response.getBody().getObject();
+    }
+
+    public static void main(String[] args) throws UnirestException {
+        System.out.println(Popstack.fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany"));
     }
 }
