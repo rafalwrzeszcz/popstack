f7f059cbd01b94dcbc4345cebd03fff6b520aa2b
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-11 10:28:52 +0100

    Initial project structure.

---
+++ src/main/java/popstack/Popstack.java
@@ -0,0 +1,14 @@
+/**
+ * This file is part of the PopStack (Java implementation).
+ *
+ * @license http://mit-license.org/ The MIT license
+ * @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+ */
+
+package popstack;
+
+public class Popstack {
+    public static void main(String[] args) {
+        System.out.println("tesT");
+    }
+}
