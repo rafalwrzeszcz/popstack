e27a6ddb9ebb4eb4d1fe2da6bd0f9ed1948281c1
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-11 23:00:38 +0100

    Executing HTTP requests.

--- src/main/kotlin/Popstack.kt
+++ src/main/kotlin/Popstack.kt
@@ -7,6 +7,17 @@
 
 package popstack;
 
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+
+fun fetch(call: String): String {
+    val (request: Request, response: Response, result: String) = Fuel
+        .get("http://api.stackexchange.com/2.2/" + call + "&site=stackoverflow")
+        .responseString();
+    return result;
+}
+
 fun main(args: Array<String>) {
-    println("Hi!");
+    println(fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany"));
 }
