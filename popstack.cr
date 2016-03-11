37005751bf396ffc47145056fe8e1e298525c61b
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-11 16:57:02 +0100

    Fetching answer.

--- popstack.cr
+++ popstack.cr
@@ -8,6 +8,17 @@
 require "http/client"
 require "json"
 
+#TODO
+# dependency management
+# code style
+# static code analysis
+# unit tests
+# auto documentation
+# exception handling
+# use more language features
+# logs
+# optimize (try to keep some parts of repetitive executions as instanced objects)
+
 #TODO: get rid of globals after redesigning into some proper project structure
 $client = HTTP::Client.new "api.stackexchange.com"
 
@@ -16,7 +27,22 @@ def fetch(call)
     return JSON.parse(response.body)
 end
 
-print(fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany")["items"])
-#TODO
+def extractSnippet(content)
+    #TODO: match, extract, trim, unescape
+    return content
+end
+
+#TODO: build query from command line arguments
+fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany")["items"].each do |item|
+    id = item["accepted_answer_id"]?
+    if id
+        print(extractSnippet(fetch("answers/" + id.to_s + "?filter=withbody")["items"][0]["body"]))
+
+        #TODO: first make sure there was a snippet extracted
+        break
+    end
+end
+
+#TODO: process more pages maybe?
 
 $client.close()
