507cda0cf85b9efa2f99f4316f61a6f874a26f44
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-11 18:48:49 +0100

    Added MIT license file.

--- popstack.cpp
+++ popstack.cpp
@@ -117,7 +117,7 @@ int main(int argc, const char* argv[]) {
                     }
                 }
 
-                //TODO; process more pages maybe?
+                //TODO: process more pages maybe?
                 return "";
         })
         .get();
