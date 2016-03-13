bd0291733ab3dfc658a1a17fb2c8614b8726776e
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-13 08:35:10 +0100

    Fetching accepted answer content.

---
+++ build.sh
@@ -0,0 +1,9 @@
+##
+# This file is part of the PopStack (D implementation).
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+##
+
+dmd popstack.d \
+    -O
--- popstack.d
+++ popstack.d
@@ -20,7 +20,24 @@ JSONValue fetch(string call) {
     return parseJSON(content);
 }
 
+string extractSnippet(string content) {
+    //TODO: match, extract, trim, unescape
+    return content;
+}
+
 void main() {
     JSONValue data = fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany")["items"];
-    writeln(data);
+    JSONValue[string] properties;
+    foreach (JSONValue item; data.array()) {
+        properties = item.object();
+        if ("accepted_answer_id" in properties) {
+            data = fetch("answers/" ~ to!string(properties["accepted_answer_id"].integer()) ~ "?filter=withbody");
+            writeln(extractSnippet(data["items"][0]["body"].str()));
+
+            //TODO: first make sure there was a snippet extracted
+            break;
+        }
+    }
+
+    //TODO: process more pages maybe?
 }
