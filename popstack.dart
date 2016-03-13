9297b3f3e7e7edbc5a4b6956d5bc5985b5cedca7
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-13 11:12:51 +0100

    Fetching answer content.

--- popstack.dart
+++ popstack.dart
@@ -10,17 +10,34 @@ import "dart:io";
 
 HttpClient client = new HttpClient();
 
-void fetch(String call) {
+void fetch(String call, Function handler) {
     client.getUrl(Uri.parse("http://api.stackexchange.com/2.2/" + call + "&site=stackoverflow"))
         .then((HttpClientRequest request) => request.close())
         .then((HttpClientResponse response) {
                 response.transform(UTF8.decoder).transform(JSON.decoder).listen((dynamic data) {
-                        print(data);
-                        //TODO
+                        handler(data);
                 });
         });
 }
 
+String extractSnippet(String content) {
+    //TODO
+    return content;
+}
+
 void main() {
-    fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany");
+    fetch("similar?order=desc&sort=relevance&title=Hibernate+manytomany", (dynamic data) {
+            for (dynamic item in data["items"]) {
+                if (item.containsKey("accepted_answer_id")) {
+                    fetch("answers/" + item["accepted_answer_id"].toString() + "?filter=withbody", (dynamic data) {
+                            print(extractSnippet(data["items"][0]["body"]));
+                    });
+
+                    //TODO: first make sure there was a snippet extracted
+                    return;
+                }
+            }
+
+            //TODO: process more pages maybe?
+    });
 }
