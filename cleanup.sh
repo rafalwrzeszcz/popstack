df832d47da564744e86f89582f25f7621dddd58f
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-13 20:53:26 +0100

    Fetching answer content.

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+##
+# This file is part of the PopStack (Bash implementation).
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+##
+
+sudo apt-get remove --purge -y jshon
+sudo apt-get autoremove --purge -y
---
+++ popstack.sh
@@ -0,0 +1,18 @@
+##
+# This file is part of the PopStack (Bash implementation).
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+##
+
+function fetch() {
+    curl -s "http://api.stackexchange.com/2.2/$1&site=stackoverflow" \
+        | gunzip
+}
+
+ANSWER_ID=`fetch "similar?order=desc&sort=relevance&title=Hibernate+manytomany" \
+    | jshon -QC -e items -a -e accepted_answer_id \
+    | head -n 1`
+
+fetch "answers/$ANSWER_ID?filter=withbody" \
+    | jshon -QC -e items -e 0 -e body -u
