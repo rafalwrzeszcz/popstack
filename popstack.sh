0320a9f982b8f79e108216b5f9d9bb1615a9d671
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-13 22:08:59 +0100

    Building query from command line arguments.

--- popstack.sh
+++ popstack.sh
@@ -5,16 +5,19 @@
 # @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
 ##
 
+#TODO: not a perferct URL encode, but should be enough for the beginning
+QUERY=`echo $@ | sed s/" "/"+"/g`
+
 function fetch() {
     curl -s "http://api.stackexchange.com/2.2/$1&site=stackoverflow" \
         | gunzip
 }
 
-ANSWER_ID=`fetch "similar?order=desc&sort=relevance&title=Hibernate+manytomany" \
+ANSWER_ID=`fetch "similar?order=desc&sort=relevance&title=$QUERY" \
     | jshon -QC -e items -a -e accepted_answer_id \
     | head -n 1`
 
 fetch "answers/$ANSWER_ID?filter=withbody" \
     | jshon -QC -e items -e 0 -e body -u \
-    | sed -n '1h;1!H;${;g;s/^.*<pre><code>\(.*\)<\/code><\/pre>.*$/\1/g;p;}' \
-    | grep -v '^$'
+    #TODO: this matched LAST, not FIRST occurance, but I guess it's fine for "MVP" ;)
+    | sed -n '1h;1!H;${;g;s/^.*<pre><code>\(.*\)<\/code><\/pre>.*$/\1/g;p;}'
