462070b3c094df30750b0a2ef56a57d7edcab291
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-20 00:26:59 +0100

    Improved overal code standards.

--- src/main/kotlin/Popstack.kt
+++ src/main/kotlin/Popstack.kt
@@ -10,10 +10,7 @@ package popstack;
 import java.net.URLEncoder;
 
 import com.github.kittinunf.fuel.Fuel;
-import com.github.kittinunf.fuel.core.Request;
-import com.github.kittinunf.fuel.core.Response;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 /*TODO:
@@ -26,21 +23,20 @@ import org.json.JSONObject;
  * use more language features (like overloaded operators)
  * logs
  * optimize
- * check if we can drop unirest to use just apache commons + org.json (or equivalent)
- * fix compiler warnings
  */
 
-val snippet: Regex = Regex("<pre><code>(.*?)</code></pre>", RegexOption.DOT_MATCHES_ALL);
+val snippet = Regex("<pre><code>(.*?)</code></pre>", RegexOption.DOT_MATCHES_ALL);
 
 fun fetch(call: String): JSONObject {
-    val (request: Request, response: Response, result: String) = Fuel
+    val result = Fuel
         .get("http://api.stackexchange.com/2.2/" + call + "&site=stackoverflow")
-        .responseString();
+        .responseString()
+        .component3();
     return JSONObject(result);
 }
 
 fun extractSnippet(content: String): String? {
-    val match: MatchResult? = snippet.find(content);
+    val match = snippet.find(content);
     if (match != null) {
         return match.groups[1]?.value?.trim()
             ?.replace("&lt;", "<")
@@ -54,24 +50,27 @@ fun extractSnippet(content: String): String? {
 }
 
 fun main(args: Array<String>): Unit {
-    val query: String = args.joinToString(" ");
+    val query = args.joinToString(" ");
 
     try {
-        var items: JSONArray = fetch("similar?order=desc&sort=relevance&title=" + URLEncoder.encode(query, "UTF-8"))
+        var items = fetch("similar?order=desc&sort=relevance&title=" + URLEncoder.encode(query, "UTF-8"))
             .getJSONArray("items");
-        var length: Int = items.length();
+        var length = items.length();
         var item: JSONObject;
-        var i: Int = 0;
+        var i = 0;
         var answer: String? = null;
         while (i < length) {
             item = items.getJSONObject(i);
 
             if (item.has("accepted_answer_id")) {
-                val id: Int = item.getInt("accepted_answer_id");
-                //TODO: don't really like that - is there any way to simply convert form Int to String?
-                items = fetch("answers/$id?filter=withbody").getJSONArray("items");
-
-                answer = extractSnippet(items.getJSONObject(0).getString("body"));
+                val id = item.getInt("accepted_answer_id");
+                answer = extractSnippet(
+                    //TODO: don't really like that - is there any way to simply convert form Int to String?
+                    fetch("answers/$id?filter=withbody")
+                        .getJSONArray("items")
+                        .getJSONObject(0)
+                        .getString("body")
+                );
 
                 if (answer != null) {
                     break;
