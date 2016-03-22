521b87fbb859f2ce36beb293d7dfa5475dd5c573
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-03-22 22:41:03 +0100

    Exception handling.

--- src/main/java/popstack/Popstack.java
+++ src/main/java/popstack/Popstack.java
@@ -29,23 +29,27 @@ import org.json.JSONObject;
  * static code analysis
  * unit tests
  * auto documentation
- * error handling
- * use more language features (like overloaded operators)
+ * use more language features
  * logs
  * optimize (try to keep some parts of repetitive executions as instanced objects)
- * check if we can drop unirest to use just apache commons + org.json (or equivalent)
  */
 
 public class Popstack {
     private static final Pattern SNIPPET = Pattern.compile("<pre><code>(.*?)</code></pre>", Pattern.DOTALL);
 
-    public static JSONObject fetch(String call) throws UnirestException {
+    public static JSONObject fetch(String call) throws UnirestException, StackOverflowException {
         HttpResponse<JsonNode> response = Unirest.get("http://api.stackexchange.com/2.2/"
                 + call
                 + "&site=stackoverflow"
             )
             .asJson();
-        return response.getBody().getObject();
+        JSONObject value = response.getBody().getObject();
+
+        if (value.has("error_message")) {
+            throw new StackOverflowException(value.getString("error_message"));
+        }
+
+        return value;
     }
 
     public static String extractSnippet(String content) {
@@ -54,32 +58,50 @@ public class Popstack {
             return StringEscapeUtils.unescapeXml(match.group(1).trim());
         }
 
-        return "";
+        return null;
     }
 
     public static void main(String[] args) throws UnirestException {
         String query = StringUtils.join(args, " ");
 
-        JSONArray items = Popstack.fetch("similar?order=desc&sort=relevance&title=" + URLParamEncoder.encode(query))
-            .getJSONArray("items");
-        int length = items.length();
-        JSONObject item;
-        for (int i = 0; i < length; ++i) {
-            item = items.getJSONObject(i);
+        try {
+            JSONArray items = Popstack.fetch("similar?order=desc&sort=relevance&title=" + URLParamEncoder.encode(query))
+                .getJSONArray("items");
+            int length = items.length();
+            JSONObject item;
+            String answer = null;
+            for (int i = 0; i < length; ++i) {
+                item = items.getJSONObject(i);
 
-            if (item.has("accepted_answer_id")) {
-                String id = String.valueOf(item.getInt("accepted_answer_id"));
-                items = Popstack.fetch("answers/" + id + "?filter=withbody").getJSONArray("items");
+                if (item.has("accepted_answer_id")) {
+                    String id = String.valueOf(item.getInt("accepted_answer_id"));
+                    items = Popstack.fetch("answers/" + id + "?filter=withbody").getJSONArray("items");
 
-                System.out.println(
-                    Popstack.extractSnippet(items.getJSONObject(0).getString("body"))
-                );
+                    answer = Popstack.extractSnippet(items.getJSONObject(0).getString("body"));
 
-                //TODO: first make sure there was a snippet extracted
-                break;
+                    if (answer != null) {
+                        break;
+                    }
+                }
             }
-        }
 
-        //TODO: process more pages maybe?
+            //TODO: process more pages maybe?
+
+            if (answer != null) {
+                System.out.println(answer);
+            } else {
+                System.out.println("Your only help is http://google.com/ man!");
+            }
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+    }
+}
+
+class StackOverflowException extends Exception {
+    private static final long serialVersionUID = 0;
+
+    public StackOverflowException(String message) {
+        super(message);
     }
 }
